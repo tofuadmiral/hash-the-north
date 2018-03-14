7773cb6d1a0cb8d5e0f23f21ec9fae51f559f26d
Ahmed Fuad Ali <fuad98ali@gmail.com>
2018-03-14 15:09:35 -0400

    added lab4 project files, including idea src code

---
+++ Lab4/src/HashTableLin.java
@@ -0,0 +1,23 @@
+public class HashTableLin {
+
+    // INSTANCE FIELDS --------------------------------------------------------------------
+
+    private Integer[] table;
+    private int size;
+    private int numkeys;
+    private double maxloadfactor;
+
+    // CONSTRUCTORS --------------------------------------------------------------------
+
+    public HashTableLin(int maxNum, double load){
+        // create an empty hash table with max load = load and maximum number of keys = maxKeys
+        // allocate the memory too so maxNum keys can fit without it exceeding maxloadfactor
+
+        maxloadfactor = load;
+        
+
+
+
+    }
+
+}
