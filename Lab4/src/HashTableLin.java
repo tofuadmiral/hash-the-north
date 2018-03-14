7f05768facef56d50b9ea7c0199021ea8d908d1d
Ahmed Fuad Ali <fuad98ali@gmail.com>
2018-03-14 15:42:34 -0400

    finished first constructor

--- Lab4/src/HashTableLin.java
+++ Lab4/src/HashTableLin.java
@@ -14,10 +14,28 @@ public class HashTableLin {
         // allocate the memory too so maxNum keys can fit without it exceeding maxloadfactor
 
         maxloadfactor = load;
-        
-
-
-
+        size = closestPrime((double)maxNum/load); // rearrange load formula
+        table = new Integer[size]; // allocate memory
     }
+
+    // function generates the smallest prime number greater than the double n
+    // otherwise, if it can't find any, returns 1
+    private int closestPrime(double n){
+        for (int i = 0; i < n+100; i++){
+            if (isPrime(i) && i>n)
+                return i;
+        }
+        return 1;
+    }
+
+    // checks if the integer passed is a prime number
+    private boolean isPrime(int n){
+        for (int i =1; i < n; i++){
+            if (n%i == 0)
+                return false;
+        }
+        return true;
+    }
+
 
 }
